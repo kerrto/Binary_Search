30d38c893dee4cae38f04b0bb7af2fd73c09dcfa
Kerry Toonen <kerrytoonen@gmail.com>
2016-01-13 06:57:31 -0500

    first commit

--- Binary_Search/main.m
+++ Binary_Search/main.m
@@ -8,10 +8,29 @@
 
 #import <Foundation/Foundation.h>
 
+
+
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
-    return 0;
+    NSArray *numbers = @[@2,@1,@3,@5,@7,@100,@1,@2,@4,@5,@10,@14,@67];
+    NSSortDescriptor *sd = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES];
+    numbers = [numbers sortedArrayUsingDescriptors:@[sd]];
+    NSUInteger searchObject = 300;
+    NSNumber *val = [NSNumber numberWithInteger:searchObject];
+    NSRange searchRange = NSMakeRange(0, [numbers count]);
+    NSUInteger findIndex = [numbers indexOfObject:val
+                                    inSortedRange:searchRange
+                                    options:NSBinarySearchingFirstEqual
+                                    usingComparator:^(id obj1, id obj2)
+                            {
+                                return [obj1 compare:obj2];
+                            }];
+
+    
+    if (findIndex > 13) {
+        return -1;}
+    
+    else
+    {
+        NSLog (@"%lu",(unsigned long)findIndex);}
 }
+
